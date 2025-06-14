88ed208699a82bbd4dac5a284d396dc2bac97b92
Anton Shiryaev <med.phisiker@gmail.com>
2025-06-15 03:36:09 +1000

    refactor(check classification): сделать рефакторинг с разбивкой по функциям
    сделать рефакторинг с разбивкой по функциям

--- document_classes.py
+++ document_classes.py
@@ -1,8 +1,7 @@
 document_classes = {
     "invoice": "Счет-фактура",
-    "new_version_tins": "ИНН_нового образца",
-    "old_version_tins": "ИНН старого образца",
+    "tin_new": "ИНН_нового образца",
+    "tin_old": "ИНН старого образца",
     "passport": "Паспорт",
-    "snils": "СНИЛС",
-    "interest_free_loan_agreement": "Договор беспроцентного займа"
+    "snils": "СНИЛС"
 }
